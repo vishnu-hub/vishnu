70a1469d5d4381226ed8131f93bc0219ed540a90
vishnu-hub <vishnupnambiar1999@gmail.com>
2020-04-06 23:28:37 +0530

    google sign in

--- app/src/main/java/com/example/googlesignin/MainActivity.java
+++ app/src/main/java/com/example/googlesignin/MainActivity.java
@@ -107,10 +107,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             GoogleSignInAccount account = result.getSignInAccount();
             String name = account.getDisplayName();
             String email = account.getEmail();
-            //String img_url = account.getPhotoUrl().toString();
             Name.setText(name);
             Email.setText(email);
-            //Glide.with(this).load(img_url).into(Prof_Pic);
             updateUI(true);
 
 
